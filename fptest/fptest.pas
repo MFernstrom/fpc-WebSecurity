250e735c8b53fc7991875e3eadb8efeee135d573
MFernstrom <Marcus@MarcusFernstrom.com>
2020-08-12 19:04:57 -0400

    Adding fptest core

---
+++ fptest/fptest.pas
@@ -0,0 +1,17 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit fptest; 
+
+interface
+
+uses
+    TestFrameworkIfaces, ProjectsManagerIface, TestListenerIface, 
+  TestFrameworkProxyIfaces, ProjectsManager, EpikTimer, TimeManager, 
+  TestFramework, XMLListener, TestFrameworkProxy, fpchelper, TextTestRunner, 
+  TestExtensions;
+
+implementation
+
+end.
